1468ce3f66a11e082ad3c79feebb3df0abd078f2
Unknown <mrm8391@rit.edu>
2019-02-07 14:30:48 -0500

    Camera initialization
    -Configure coordinate system for camera
    -Set up other variables camera will use

--- Raytracer/include/renderer/Camera.h
+++ Raytracer/include/renderer/Camera.h
@@ -5,10 +5,38 @@ Camera object, for defining the viewing perspective.
 #ifndef RT_CAMERA_H
 #define RT_CAMERA_H
 
+#include <vector>
+
+#include <renderer/Point.h>
+#include <renderer/Vector.h>
+#include <prims/Object.h>
+
 class Camera {
 
 public:
-	Camera();
+	/*
+	Construct a camera and coordinate system from given variables.
+
+	@param pos Point location of the camera
+	@param look Direction camera is pointing at
+	@param base Arbitrary vector to base coordinate system off of (NOT up).
+				Will be used to compute unit vector v, which will be used
+				to compute the up vector. Can be the same as up, but not necessary.
+	*/
+	Camera(Point pos, Vector look, Vector base);
+
+	std::vector<Object> objects;
+	Point eyepoint;
+	Vector lookat;
+	Vector up;
+
+	//Unit vectors defining camera coordinate space.
+	//n is analogous to lookat, u to up, and v is orthogonal to both
+	Vector n, u, v;
+
+	//Virtual screen in front of camera.
+	//Placeholder; will contain colors, and later irradiance
+	std::vector<std::vector<double>> screen;
 };
 
 #endif
--- Raytracer/src/Raytracer.cpp
+++ Raytracer/src/Raytracer.cpp
@@ -3,11 +3,30 @@
 
 #include <iostream>
 
+#include <renderer/Camera.h>
+#include <renderer/Point.h>
+#include <renderer/Vector.h>
+
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+
 using namespace std;
+using Eigen::MatrixXd;
+using Eigen::Matrix;
+using Eigen::StorageOptions;
+
+using namespace Eigen;
+using Eigen::Vector3d;
 
 int main()
 {
-	cout << "Hello World!" << endl;
+	Point origin(0, 0, 0);
+	Vector look(1, 0, 0);
+	Vector up(0, 1, 0);
+
+	Camera cam(origin, look, up);
+
+
 
     return 0;
 }
