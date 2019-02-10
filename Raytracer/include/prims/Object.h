d4572df9c19a19a2dfea69f813fe877d05c56792
Unknown <mrm8391@rit.edu>
2019-02-10 16:56:34 -0500

    Add more information to Object
    -Assign unique IDs
    -Add copy constructor (which does not assign new id)
    -Add stubs for transforming objects

--- Raytracer/include/prims/Object.h
+++ Raytracer/include/prims/Object.h
@@ -16,12 +16,34 @@ These will be mathematically defined.
 class Object {
 
 public:
+	static unsigned int NEXT_ID;
+
 	Point pos;
 
 	// For now, a 0-1 value
 	double color;
+	unsigned int id;
 
+	/*
+	Default constructor, placing object at origin and assigning a unique ID.
+
+	Only intended to be used by derivative classes, object shouldn't
+	be constructed alone.
+	*/
 	Object();
+
+	/*
+	Construct an object at a specified position, and assign it a unique ID.
+	*/
+	Object(double x, double y, double z);
+
+	/*
+	Copy constructor; create new object from an old one. Does not assign a new ID,
+	copies ID from old object.
+
+	@param other Object to copy
+	*/
+	Object(Object &other);
 
 	// Returns array of intersections with that object
 	virtual IntersectData* intersect(Ray ray);
@@ -29,6 +51,28 @@ public:
 	virtual void translate(double x, double y, double z);
 
 	virtual void move(double x, double y, double z);
+
+	/*
+	Transform object by an arbitrary transformation matrix, typically a view transform.
+	Modifies current object.
+
+	For now, this will only make sense for 2D objects (like triangles) or objects that are rotationally symmetric (like spheres).
+	Will have to find a solution for other cases later.
+
+	@param matr Transformation matrix to apply to the object
+	*/
+	virtual void transform(Eigen::Matrix4d matr);
+
+	/*
+	Copy object, and transform it with a transformation matrix. Intended for use when
+	transforming world objects into camera space. New object will not have a unique ID, will
+	have the ID of this object.
+
+	@param matr Transformation matrix to apply to the object
+
+	@return New, transformed version of current object. Transformed object will have the same ID.
+	*/
+	virtual Object transformAndCopy(Eigen::Matrix4d matr);
 };
 
 #endif
--- Raytracer/src/prims/Object.cpp
+++ Raytracer/src/prims/Object.cpp
@@ -1,7 +1,23 @@
 #include <prims/Object.h>
 
-Object::Object()
+unsigned int Object::NEXT_ID = 0;
+
+Object::Object() : Object(0,0,0)
 {
+	
+}
+
+Object::Object(double x, double y, double z) {
+	this->id = Object::NEXT_ID;
+	Object::NEXT_ID++;
+
+	this->pos = Point(x, y, z);
+}
+
+Object::Object(Object & other)
+{
+	this->id = other.id;
+	this->pos = Point(other.pos);
 }
 
 IntersectData * Object::intersect(Ray ray)
@@ -15,4 +31,13 @@ void Object::translate(double x, double y, double z)
 
 void Object::move(double x, double y, double z)
 {
+}
+
+void Object::transform(Eigen::Matrix4d matr)
+{
+}
+
+Object Object::transformAndCopy(Eigen::Matrix4d matr)
+{
+	return Object();
 }
