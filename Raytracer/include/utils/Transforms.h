4589af4b8b1c26307b78b3bb52f429230ff4c324
Unknown <mrm8391@rit.edu>
2019-02-10 13:40:59 -0500

    View transform construction

--- Raytracer/include/utils/Transforms.h
+++ Raytracer/include/utils/Transforms.h
@@ -27,6 +27,27 @@ public:
 	*/
 	static Eigen::Matrix4d translate(double dx, double dy, double dz);
 
+	/*
+	Return a camera view transform http://www.codinglabs.net/article_world_view_projection_matrix.aspx
+
+	(Column major)
+	[ [X.x, x.y, x.z, 0]
+	  [Y.x, Y.y, Y.z, 0]
+	  [Z.x, Z.y, Z.z, 0]
+	  [t.x, t.y, t.z, 1] ]
+
+	@param X X-axis of camera space in world coordinates
+	@param Y Y-axis of camera space in world coordinates
+	@param Z Z-axis of camera space in world coordinates
+	@param t Translation to camera position
+	*/
+	static Eigen::Matrix4d viewTransform(
+		Eigen::Vector3d X,
+		Eigen::Vector3d Y,
+		Eigen::Vector3d Z,
+		Eigen::Vector3d t
+	);
+
 private:
 	//Private constructor to prevent initialization
 	Transforms();
--- Raytracer/src/utils/Transforms.cpp
+++ Raytracer/src/utils/Transforms.cpp
@@ -36,3 +36,23 @@ Eigen::Matrix4d Transforms::translate(double dx, double dy, double dz)
 
 	return t.matrix();
 }
+
+Eigen::Matrix4d Transforms::viewTransform(Eigen::Vector3d X, Eigen::Vector3d Y, Eigen::Vector3d Z, Eigen::Vector3d t)
+{
+	//Construct a matrix using Eigen's block operations to splice
+	//the vectors in. Can't use nicer syntax since we're converting
+	//3d vectors into a 4d matrix, but this gets optimized by compiler
+	Matrix4d m;
+	m.block<3, 1>(0, 0) = X;
+	m.block<3, 1>(0, 1) = Y;
+	m.block<3, 1>(0, 2) = Z;
+	m.block<3, 1>(0, 3) = t;
+
+	//Set bottom row and homogeneous coordinate
+	m(3, 0) = 0;
+	m(3, 1) = 0;
+	m(3, 2) = 0;
+	m(3, 3) = 1;
+
+	return m;
+}
