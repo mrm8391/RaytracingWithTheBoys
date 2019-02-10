f644477a97c30395c66a59b1c8882e1ee5abef71
Unknown <mrm8391@rit.edu>
2019-02-10 15:33:49 -0500

    Camera view transform, world origin
    -Initialize camera's view transform
    -Add origin and initializer to world
    -Add crude camera space test in main file

--- Raytracer/include/renderer/World.h
+++ Raytracer/include/renderer/World.h
@@ -7,13 +7,24 @@ Model for a scene's world in the renderer.
 
 #include <vector>
 
-#include "../prims/Object.h"
+#include <prims/Object.h>
+#include <renderer/Point.h>
 
 class World {
 	
 public:
-	World();
 	std::vector<Object> objects;
+	Point origin;
+	
+	/*
+	Default constructor, creating a world with origin (0,0,0)
+	*/
+	World();
+
+	/*
+	Create a world with an alternative origin if feeling masochistic
+	*/
+	World(double x, double y, double z);
 };
 
 
