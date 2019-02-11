27fc2fa4a8daf48c47445f2b5712c0b011f04aac
Unknown <mrm8391@rit.edu>
2019-02-11 12:16:01 -0500

    Scene placement wip

--- Raytracer/src/Raytracer.cpp
+++ Raytracer/src/Raytracer.cpp
@@ -30,20 +30,25 @@ int main()
 	Eigen::Vector4d test;
 	test << 0, 0, 0, 1;
 
-	Point f1(-100, -100, 0), f2(-100, 100, 0), f3(100, -100, 0), f4(0, 10, 0);
+	Point f1(0, 0, 0), f2(0, 0, 50), f3(50, 0, 0), f4(50, 50, 0);
 	Triangle* floor1 = new Triangle(f1, f2, f3);
 	Triangle* floor2 = new Triangle(f4, f2, f1);
+	floor1->color = .75;
+	floor2->color = .75;
 
 	/*Point s1(0, 0, 0);
 	Sphere sphere1(s1, 2.0);
 	sphere1.color = 0.5;*/
 
+	World w;
 	w.addObject(floor1);
 	//w.addObject(floor2);
 	//w.addObject(sphere1);
 
-	Point camOrig(0, 0, 20);
-	Vector lookat(0, 0, 0), base(0, 1, 0);
+	Point camOrig(0, 0, -10);
+	Point worldOrig(0, 0, 0);
+	Vector lookat = worldOrig.subtract(camOrig);
+	Vector base(0, 1, 0);
 
 	Camera cam(camOrig, lookat, base);
 
